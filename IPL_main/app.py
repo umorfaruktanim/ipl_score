1ab5820c8da4a490991dc756297c604478b0fc77
umorfaruktanim <66315556+umorfaruktanim@users.noreply.github.com>
2025-07-07 19:11:57 +0600

    Update app.py

--- IPL_main/app.py
+++ IPL_main/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import numpy as np
+import pandas as pd
 import joblib
 import tensorflow as tf
 
@@ -15,7 +16,6 @@ striker_encoder = joblib.load("striker_encoder.pkl")
 bowler_encoder = joblib.load("bowler_encoder.pkl")
 
 # Load data for dropdowns
-import pandas as pd
 ipl = pd.read_csv("ipl_data.csv")
 df = ipl.drop(['date', 'runs', 'wickets', 'overs', 'runs_last_5', 'wickets_last_5','mid', 'striker', 'non-striker'], axis=1)
 
@@ -31,21 +31,18 @@ def index():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    # Get values from form
     venue = request.form['venue']
     bat_team = request.form['bat_team']
     bowl_team = request.form['bowl_team']
     batsman = request.form['batsman']
     bowler = request.form['bowler']
 
-    # Encode
     venue = venue_encoder.transform([venue])[0]
     bat_team = batting_team_encoder.transform([bat_team])[0]
     bowl_team = bowling_team_encoder.transform([bowl_team])[0]
     batsman = striker_encoder.transform([batsman])[0]
     bowler = bowler_encoder.transform([bowler])[0]
 
-    # Prepare and predict
     input_data = np.array([[venue, bat_team, bowl_team, batsman, bowler]])
     input_data = scaler.transform(input_data)
     prediction = model.predict(input_data)[0][0]
